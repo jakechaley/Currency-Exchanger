4cc4287f63673e43d4a4c82fc598860b0ebb3d38
jakechaley <jakechaley@gmail.com>
2022-02-11 21:23:49 -0800

    update styling/fix js error message

--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from "./currency";
+import currencySymbols from "./currencySymbols";
 
 $(document).ready(function() {
   $("#userInput").submit(function(event) {
     event.preventDefault();
-    const currencyCode = $("#currencyChoice").val();
+    const currencyCode = $("#currencyChoice").val().toUpperCase();
     const dollarInput = $("#dollarAmount").val();
     $("#currencyChoice").val("");
     $("#dollarAmount").val("");
     $(".showErrors").text("");
     $(".exchangeResults").text('');
     let promise = Currency.getRate(currencyCode, dollarInput);
-    promise.then(function (response) {
-      const body = JSON.parse(response);
-      $(".exchangeResults").text(`The amount you entered is equal to ${body.conversion_result.toFixed(2)} ${currencyCode.toUpperCase()}`);
-    })
+    promise
+      .then(function (response) {
+        const body = JSON.parse(response);
+        const symbols = currencySymbols[currencyCode].symbol;
+        $(".exchangeResults").text(`$${dollarInput} is equal to ${symbols}${body.conversion_result.toFixed(2)}`);
+      })
       .catch(function(error) {
-        $(".showErrors").text(`There was an error processing your request: ${error}`);
+        const object = JSON.parse(error);
+        $(".showErrors").text(`There was an error processing your request: ${object['error-type']}`);
       });
   });
 });
